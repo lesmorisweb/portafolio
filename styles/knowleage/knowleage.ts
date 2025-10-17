8e260429622e6aed81c9ef95174b7eec464263ea
Laura Esmoris <lesmoris0web@gmail.com>
2025-10-17 16:36:51 -0500

    structure done

--- styles/knowleage/knowleage.ts
+++ styles/knowleage/knowleage.ts
@@ -1,6 +1,6 @@
 const goBackIndex= document.querySelector(".goBackArrow") as HTMLElement
 
 
-goBackIndex?.addEventListener("click", ()=>{
+goBackIndex.addEventListener("click", ()=>{
     window.location.href="./index.html"
 })
