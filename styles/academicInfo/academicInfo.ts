6bda5a33780b060b9b13d472885642485a5fef8a
Laura Esmoris <lesmoris0web@gmail.com>
2025-10-24 15:42:32 -0500

    go back fix

--- styles/academicInfo/academicInfo.ts
+++ styles/academicInfo/academicInfo.ts
@@ -1,5 +1,5 @@
-const goBackToIndex= document.querySelector("goBackArrow") as HTMLElement
+const goBackToIndex= document.querySelector(".goBackArrow") as HTMLElement
 
 goBackToIndex.addEventListener("click", ()=> {
-    window.location.href= "index.html"
+    window.location.href= "./index.html"
 })
